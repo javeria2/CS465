126f37fc14465e5303d56896f270db848558b404
Zach Brewer <zbrewer02@gmail.com>
2016-11-13 17:35:35 -0600

    Should now be able to add an empty linear layout to the message board. Need to add the text views to it.

--- app/src/main/java/edu/illinois/cs465/parkingpterodactyl/MessageBoard.java
+++ app/src/main/java/edu/illinois/cs465/parkingpterodactyl/MessageBoard.java
@@ -6,8 +6,41 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.LinkedList;
 
 public class MessageBoard extends AppCompatActivity {
+
+    private int dpToPx(int dp) {
+        float density = getApplicationContext().getResources().getDisplayMetrics().density;
+        return (int)(density * dp);
+    }
+
+    private void addMessageToBoard(Message m) {
+        /*******************************************************************************************
+         * Create the linear layout to hold the message
+         ******************************************************************************************/
+        LinearLayout messagesContainer = (LinearLayout)findViewById(R.id.content_message_board);
+
+        LinearLayout newContainer = new LinearLayout(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(0, 0, 0, dpToPx(4));
+        newContainer.setLayoutParams(layoutParams);
+        newContainer.setOrientation(LinearLayout.HORIZONTAL);
+        newContainer.setBackgroundResource(R.drawable.message_board_border);
+
+        /*******************************************************************************************
+         * Create the text view with the message text
+         ******************************************************************************************/
+
+        /*******************************************************************************************
+         * Create the text view with the time text
+         ******************************************************************************************/
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +57,10 @@ public class MessageBoard extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        LinkedList<Message> messages = new LinkedList<Message>();
+        messages.add(new Message("Test message"));
+
     }
 
 }
