6bf5f6611cc75887cbd6efa17fef7162feea77c2
pranaviy <pyalama2@illinois.edu>
2016-11-13 15:34:33 -0600

    Added navigation and list activity

---
+++ app/src/main/java/edu/illinois/cs465/parkingpterodactyl/ParkingList.java
@@ -0,0 +1,13 @@
+package edu.illinois.cs465.parkingpterodactyl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParkingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parking_list);
+    }
+}
