a0e604e46364acd353dbd359eb56b9a7a76edcd1
Sanchay Javeria <javeria2@illinois.edu>
2016-11-13 18:58:19 -0600

    images added to select car sizes

--- app/src/main/java/edu/illinois/cs465/parkingpterodactyl/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/parkingpterodactyl/MainActivity.java
@@ -1,15 +1,20 @@
 package edu.illinois.cs465.parkingpterodactyl;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
