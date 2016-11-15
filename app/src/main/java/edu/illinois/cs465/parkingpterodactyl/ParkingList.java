18e8d6c2fe6d19474c039230dfb210317ebb960d
pranaviy <pyalama2@illinois.edu>
2016-11-14 23:53:22 -0600

    Added to Parking list

--- app/src/main/java/edu/illinois/cs465/parkingpterodactyl/ParkingList.java
+++ app/src/main/java/edu/illinois/cs465/parkingpterodactyl/ParkingList.java
@@ -1,7 +1,10 @@
 package edu.illinois.cs465.parkingpterodactyl;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class ParkingList extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class ParkingList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+    }
+
+    public void buttonPress(View v) {
+        switch (v.getId()) {
+            case R.id.navigate:
+                startNavigation();
+                break;
+        }
+    }
+
+    public void startNavigation() {
+        Intent intent = new Intent(this, Navigation.class);
+        startActivity(intent);
     }
 }
