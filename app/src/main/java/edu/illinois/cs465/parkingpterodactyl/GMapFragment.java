4d7ce3789ac2e713c9a963febb5b727e3abd0df4
Sanchay Javeria <javeria2@illinois.edu>
2016-11-15 22:38:29 -0600

    markers added

--- app/src/main/java/edu/illinois/cs465/parkingpterodactyl/GMapFragment.java
+++ app/src/main/java/edu/illinois/cs465/parkingpterodactyl/GMapFragment.java
@@ -12,6 +12,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -34,9 +36,18 @@ public class GMapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         gmap = googleMap;
-        LatLng sydney = new LatLng(40.1319, -88.2952);
-        gmap.addMarker(new MarkerOptions().position(sydney).title("Marker in sydney"));
-        gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 17));
+
+        addCustomMarkers(R.drawable.pin4, 40.1104619, -88.230041);
+        addCustomMarkers(R.drawable.pin2, 40.110109, -88.2303623);
+        addCustomMarkers(R.drawable.pin3, 40.110363, -88.230620);
+        addCustomMarkers(R.drawable.pin5, 40.110072, -88.231741);
+        addCustomMarkers(R.drawable.pin1, 40.109871, -88.228367);
+        addCustomMarkers(R.drawable.pin6, 40.110790, -88.229032);
+        addCustomMarkers(R.drawable.pin7, 40.110634, -88.230207);
+
+        LatLng pin = new LatLng(40.1104619, -88.230041);
+
+        gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(pin, 17));
         if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -60,6 +71,15 @@ public class GMapFragment extends Fragment implements OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState);
         MapFragment fragment = (MapFragment)getActivity().getFragmentManager().findFragmentById(R.id.map);
         fragment.getMapAsync(this);
+    }
+
+    //this function adds a new marker on the map
+    public void addCustomMarkers(int resource, double lat, double lon) {
+        LatLng pin = new LatLng(lat,lon);
+        gmap.addMarker(new MarkerOptions()
+                .position(pin)
+                .title("Marker in sydney")
+                .icon(BitmapDescriptorFactory.fromResource(resource)));
     }
 
 }
