8f4a412fc1545ecece1e5ee4501a5db1c417ab10
Sanchay Javeria <javeria2@illinois.edu>
2016-11-15 22:14:28 -0600

    satellite view removed

--- app/src/main/java/edu/illinois/cs465/parkingpterodactyl/GMapFragment.java
+++ app/src/main/java/edu/illinois/cs465/parkingpterodactyl/GMapFragment.java
@@ -1,6 +1,8 @@
 package edu.illinois.cs465.parkingpterodactyl;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +16,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class GMapFragment extends Fragment implements OnMapReadyCallback{
+public class GMapFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap gmap;
 
     public GMapFragment() {
@@ -32,10 +34,21 @@ public class GMapFragment extends Fragment implements OnMapReadyCallback{
     @Override
     public void onMapReady(GoogleMap googleMap) {
         gmap = googleMap;
-        LatLng sydney = new LatLng(10.762984, 106.682329);
+        LatLng sydney = new LatLng(40.1319, -88.2952);
         gmap.addMarker(new MarkerOptions().position(sydney).title("Marker in sydney"));
         gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 17));
-        gmap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        gmap.setMyLocationEnabled(true);
+
     }
 
     public View onCreateVew(LayoutInflater inflater, ViewGroup container,
